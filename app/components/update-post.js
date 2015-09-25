1f2017df3ae5c38972c534d8ede64c6f07d45248
Evan Clough <clough.evan@gmail.com>
2015-09-25 09:04:14 -0700

    Add actions to update-post component.

--- app/components/update-post.js
+++ app/components/update-post.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updatePostForm: false,
+  actions: {
+    updatePostForm() {
+      this.set('updatePostForm', true);
+    },
+    updatePost(post) {
+      var params = {
+        author: this.get('author'),
+        title: this.get('title'),
+        body: this.get('body'),
+        location: this.get('location'),
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('updatePost', post, params);
+    },
+  }
 });
