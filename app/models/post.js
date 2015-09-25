96eec4ae9bcc87ff8eaefb9259238d02aad0031d
Evan Clough <clough.evan@gmail.com>
2015-09-25 08:23:55 -0700

    Add attributes to post and comment models.

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  
+  image: DS.attr(),
+  title: DS.attr(),
+  caption: DS.attr(),
+  comments: DS.hasMany('comment', { async: true }),
 });
