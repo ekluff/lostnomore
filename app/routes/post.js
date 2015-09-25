e32f1d5f25b21a2cdbb9bb29f02e2f3f8d557879
Evan Clough <clough.evan@gmail.com>
2015-09-25 08:56:36 -0700

    Add actions to index and post routes.

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,4 +1,31 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('post', params.post_id);
+  },
+  actions: {
+    // save new comments
+    saveComment(params) {
+      var newComment = this.store.createRecord('comment', params);
+      newComment.save();
+      params.post.save();
+      this.transitionTo('post', params.post);
+    },
+    // delete post
+    destroyPost(post) {
+      post.destroyRecord();
+      this.transitionTo('index');
+    },
+    // update post
+    updatePost(post, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key] !== undefined) {
+          post.set(key, params[key]);
+        }
+      });
+      post.save();
+      this.transitionTo('post', params.post);
+    },
+  }
 });
