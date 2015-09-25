3d92af23f54ebf63200fc52c9bc3e009eec323e6
Evan Clough <clough.evan@gmail.com>
2015-09-25 10:37:55 -0700

    Update models with current attributes.

--- app/models/post.js
+++ app/models/post.js
@@ -1,8 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  image: DS.attr(),
+  author: DS.attr(),
+  location: DS.attr(),
   title: DS.attr(),
-  caption: DS.attr(),
+  body: DS.attr(),
   comments: DS.hasMany('comment', { async: true }),
 });
