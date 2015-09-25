08d6ad9d770c13b5a81ad42da29f839d1c4df541
Evan Clough <clough.evan@gmail.com>
2015-09-25 09:39:28 -0700

    Configure firebase URL.

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'lostnomore',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://lostnomore.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
